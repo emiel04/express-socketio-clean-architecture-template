4e46c22d6d0ce60459398d13def55796f7e3e7d0
Emiel <emiel.delaey@live.be>
2025-01-30 11:47:17 +0100

    change name to presenter for clarity

--- src/infrastructure/setup/endpoints/HealthEndpoint.ts
+++ src/infrastructure/setup/endpoints/HealthEndpoint.ts
@@ -1,13 +1,13 @@
 import type { Endpoint } from "@infrastructure/shared/endpoint";
-import { HealthOutputPort } from "@infrastructure/setup/outputs/HealthOutputPort";
+import { HealthPresenter } from "@infrastructure/setup/outputs/HealthPresenter";
 import { HealthUseCase } from "@application/use-cases/HealthUseCase";
 import { HealthController } from "@infrastructure/controller/Http/HealthController";
 import type { Request, Response } from "express";
 
 export class HealthEndpoint implements Endpoint {
     async handle(req: Request, res: Response): Promise<void> {
-        const outputPort = new HealthOutputPort(res);
-        const useCase = new HealthUseCase(outputPort);
+        const presenter = new HealthPresenter(res);
+        const useCase = new HealthUseCase(presenter);
         const controller = new HealthController(useCase);
 
         await controller.handle();
--- src/infrastructure/setup/outputs/HealthPresenter.ts
+++ src/infrastructure/setup/outputs/HealthPresenter.ts
@@ -2,7 +2,7 @@ import type { OutputPort } from "@application/shared/OutputPort";
 import type { HealthOutput } from "@application/use-cases/HealthUseCase";
 import type { Response } from "express";
 
-export class HealthOutputPort implements OutputPort<HealthOutput> {
+export class HealthPresenter implements OutputPort<HealthOutput> {
     private response: Response;
 
     constructor(response: Response) {
